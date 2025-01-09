135dc4f98bd55ce47437dbed844f1532b4740b89
ElieOko <elieoko100@gmail.com>
2025-01-09 12:38:52 +0100

    feat:method isPermissionGranted and type return value Boolean

--- app/src/main/java/com/master/masteradaptaterbleutooth/core/permission/MultiplePermissionAndroid.kt
+++ app/src/main/java/com/master/masteradaptaterbleutooth/core/permission/MultiplePermissionAndroid.kt
@@ -2,8 +2,14 @@ package com.master.masteradaptaterbleutooth.core.permission
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.core.content.ContextCompat
 
-private fun isPermissionGranted(name: String, context: Context) = ContextCompat.checkSelfPermission(
+/**
+ * @author Elie Oko
+ */
+private fun isPermissionGranted(name: String, context: Context):Boolean =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+    ContextCompat.checkSelfPermission(
     context, name
-) == PackageManager.PERMISSION_GRANTED
+) == PackageManager.PERMISSION_GRANTED else true
